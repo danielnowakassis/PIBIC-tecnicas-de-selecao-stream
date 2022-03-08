e7dbb675dfb60ce2dd4df2dfd1a3bc4de431090a
danielnowakassis <daniel.nowak.assis@gmail.com>
2022-03-08 10:24:42 -0300

    Update DSARF.java

--- DSARF.java
+++ DSARF.java
@@ -1,6 +1,8 @@
 package moa.classifiers.meta;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.ExecutorService;
 
 import com.github.javacliparser.IntOption;
 import com.yahoo.labs.samoa.instances.Instance;
@@ -8,8 +10,11 @@ import com.yahoo.labs.samoa.instances.Instance;
 import moa.AbstractMOAObject;
 import moa.classifiers.core.driftdetection.ChangeDetector;
 import moa.classifiers.meta.AdaptiveRandomForest.ARFBaseLearner;
+import moa.classifiers.meta.AdaptiveRandomForest.TrainingRunnable;
 import moa.classifiers.trees.ARFHoeffdingTree;
 import moa.core.DoubleVector;
+import moa.core.InstanceExample;
+import moa.core.MiscUtils;
 import moa.core.Utils;
 import moa.evaluation.BasicClassificationPerformanceEvaluator;
 import moa.options.ClassOption;
@@ -18,6 +23,8 @@ public class DSARF extends AdaptiveRandomForest {
 	private static final long serialVersionUID = 1L;
 	public ArrayList<ArrayList<Integer>> ensemblearray;
 	public static boolean changetobkg = false;
+	public static int acertos;
+	public static int acuracia;
 	public IntOption slidingWindowSizeOption = new IntOption("slidingWindowSize", 'z',
             "Size of the sliding window", 100, 1, Integer.MAX_VALUE);
 	
@@ -26,19 +33,39 @@ public class DSARF extends AdaptiveRandomForest {
         return "Dynamic Selection in Adaptive Random Forest algorithm for evolving data streams";
     }
 	
-	/*
-	//slidingdelayedwindow
-	public static ArrayList<Integer> slidingdelayedwindowarray(ArrayList<Integer> array, boolean correctlyClassifies, int windowsize){
-        int b = correctlyClassifies? 1 : 0;
-        array.add(b);
-        if (array.size() % 50 == 0 && array.size() > 50){
-            for (int i = 0; i < 50; i++){
-                array.remove(i);
+	private ExecutorService executor;
+	
+	@Override
+    public void trainOnInstanceImpl(Instance instance) {
+        ++this.instancesSeen;
+        if(this.ensemble == null) 
+            initEnsemble(instance);
+        Collection<TrainingRunnable> trainers = new ArrayList<TrainingRunnable>();
+        for (int i = 0 ; i < this.ensemble.length ; i++) {
+            DoubleVector vote = new DoubleVector(this.ensemble[i].getVotesForInstance(instance));
+            InstanceExample example = new InstanceExample(instance);
+            this.ensemble[i].evaluator.addResult(example, vote.getArrayRef());
+            int k = MiscUtils.poisson(this.lambdaOption.getValue(), this.classifierRandom);
+            if (k > 0) {
+                if(this.executor != null) {
+                    TrainingRunnable trainer = new TrainingRunnable(this.ensemble[i], 
+                        instance, k, this.instancesSeen);
+                    trainers.add(trainer);
+                }
+                else { // SINGLE_THREAD is in-place... 
+                    this.ensemble[i].trainOnInstance(instance, k, this.instancesSeen);
+                }
             }
         }
-        return array;
+        if(this.executor != null) {
+            try {
+                this.executor.invokeAll(trainers);
+            } catch (InterruptedException ex) {
+                throw new RuntimeException("Could not call invokeAll() on training threads.");
+            }
+        }
     }
-    */
+	
 	@Override
     public double[] getVotesForInstance(Instance instance) {
 		Instance testInstance = instance.copy();
@@ -48,7 +75,6 @@ public class DSARF extends AdaptiveRandomForest {
         	this.ensemblearray = new ArrayList<ArrayList<Integer>>();
         }
         DoubleVector combinedVote = new DoubleVector();
-         
         for(int i = 0 ; i < this.ensemble.length ; ++i) {
         	//add ensemble array
         	if (changetobkg) {
@@ -59,13 +85,27 @@ public class DSARF extends AdaptiveRandomForest {
         		ArrayList<Integer> array = new ArrayList<Integer>();
         		this.ensemblearray.add(array);
         	}
+        	//this.ensemblearray.get(i);
+        	/*
+        	try {
+        		this.ensemblearray.get(i);
+        	}catch(){
+        		ArrayList<Integer> array = new ArrayList<Integer>();
+        		this.ensemblearray.add(array);
+        	}*/
+        	
         	//slidingwindow
         	boolean correctlyClassifies = this.ensemble[i].classifier.correctlyClassifies(instance);
         	int b = correctlyClassifies? 1 : 0;
-        	this.ensemblearray.get(i).add(b);
+        	this.ensemblearray.get(i).add(b); 
             if(this.ensemblearray.get(i).size() == this.slidingWindowSizeOption.getValue() + 1){	           	
             	this.ensemblearray.get(i).remove(0);	
             }
+            for(int j = 0; j < this.ensemblearray.get(i).size(); ++j) {
+		    	acertos += this.ensemblearray.get(i).get(j);
+		    }
+		    float acuracia = (float) acertos/ this.ensemblearray.get(i).size();
+		    acertos = 0;  
             DoubleVector vote = new DoubleVector(this.ensemble[i].getVotesForInstance(testInstance));
             if (vote.sumOfValues() > 0.0) {
                 vote.normalize();
